dd53ba598a4c49fb226e5fc2a388f2176bbf59f5
Nischay Venkatram <nischay.v@gmail.com>
2016-11-18 01:21:52 -0600

    Added yarnclean to gitignore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 'use strict';
+
 import got from 'got';
-import promise from 'pinkie-promise';
+import Promise from 'pinkie-promise';
 
 function getUser(username) {
     if (typeof username !== 'string') {
-        return promise.reject(new Error('username required'));
+        return Promise.reject(new Error('username required'));
     }
 
 }
